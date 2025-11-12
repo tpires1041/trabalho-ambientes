67fa1387d52b03a3c55bb845837213e15582c2bc
tpires1041 <tiagopires2501@outlook.com>
2025-11-12 16:00:57 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,22 @@
+print("===============================")
+print("         JOGO DA FORCA")
+print("===============================\n")
+
+print("1 - Jogar")
+print("2 - Selecionar dificuldade")
+print("3 - Sair\n")
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+    print("\nIniciando o jogo... Boa sorte!")
+    
+    print("  _______")
+    print(" |/      |")
+    print(" |      (_)")
+    print(" |      \\|/")
+    print(" |       |")
+    print(" |      / \\")
+    print(" |")
+    print("_|___")
+    
