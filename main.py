8283948b2f7785eab2e5114d40ddca4a0ffdf7b2
Jordana <jordanameldantas@gmail.com>
2025-11-17 16:43:07 -0300

    Tema animais
     adicionei menu c opção de escolher se será palavra ou letra, aparece a cada chute

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 import random
 
-print("===============================")
-print("          JOGO DA FORCA")
-print("===============================\n")
+print("------------------------------")
+print("|        JOGO DA FORCA       |")
+print("------------------------------\n")
 
 print("Escolha um tema")
 print("1 - Profissões")
 print("2 - Objetos")
 print("3 - Meios de transporte")
-print("4 - ")
+print("4 - Animais")
 print("5 - Sair\n")
 
 opcao = input("Escolha uma opção: ")
 
-temaProfissoes = ["ADVOGADO", "PILOTO", "MEDICO", "ENGENHEIRO", "PROFESSOR", "PROGRAMADOR", "ENFERMEIRO", "FARMACEUTICO", "ARQUITETO", "DESIGNER", "MOTORISTA", "VENDEDOR", "GERENTE", "ARTISTA", "PSICOLOGO", "BIOLOGO", "QUIMICO", "FISICO", "ATLETA", "ATENDENTE"]
+temaProfissoes = ["ADVOGADO", "PILOTO", "MEDICO", "ENGENHEIRO", "PROFESSOR", "PROGRAMADOR",
+                  "ENFERMEIRO", "FARMACEUTICO", "ARQUITETO", "DESIGNER", "MOTORISTA",
+                  "VENDEDOR", "GERENTE", "ARTISTA", "PSICOLOGO", "BIOLOGO", "QUIMICO",
+                  "FISICO", "ATLETA", "ATENDENTE"]
 
 temaObjetosdia = ["SOFA", "COPO", "TOALHA", "PRATO", "CHAVE", "PASTA",
-        "BOLSA", "ESPELHO", "GARRAFA", "TALHER", "CARREGADOR",
-        "POTE", "PERFUME"]
-        
-temaTransporte = ["CARRUAGEM", "CANOA", "BONDE", "TUK-TUK", "BALAO", "CAMELO", "CAVALO", "TELEFERICO", "SUBMARINO", "BALSA", "FOGUETE", "LAMBRETA", "PLANADOR"]
+                  "BOLSA", "ESPELHO", "GARRAFA", "TALHER", "CARREGADOR",
+                  "POTE", "PERFUME"]
+
+temaTransporte = ["CARRUAGEM", "CANOA", "BONDE", "TUKTUK", "BALAO", "CAMELO", "CAVALO",
+                  "TELEFERICO", "SUBMARINO", "BALSA", "FOGUETE", "LAMBRETA", "PLANADOR"]
+
+temaAnimais = ["CACHORRO", "GATO", "ELEFANTE", "TARTARUGA", "PATO", "ARARA", "MACACO",
+               "GIRAFA", "TIGRE", "LEAO", "PANTERA"]
 
 if opcao == "1":
-    palavra = random.choice(temaProfissoes)
-
+        palavra = random.choice(temaProfissoes)
 elif opcao == "2":
-    palavra = random.choice(temaObjetosdia)
-
+        palavra = random.choice(temaObjetosdia)
 elif opcao == "3":
     palavra = random.choice(temaTransporte)
+elif opcao == "4":
+    palavra = random.choice(temaAnimais)
+elif opcao == "5":
+    print("Saindo...")
+    exit()
+else:
+    print("Opção inválida!")
+    exit()
 
-print("\nTema selecionado! Vamos jogar!\n")
+print("\nPalavra selecionada! Vamos jogar!\n")
 
 print("  _______")
 print(" |/      |")
@@ -41,54 +54,87 @@ print(" | ")
 print(" |")
 print("_|___ \n")
 
-palavraUsuario = []
-for i in range(len(palavra)):
-    palavraUsuario.append("_")
+palavraUsuario = ["_"] * len(palavra)
 
-print("Palavra:\n")
-for letra in palavraUsuario:
-    print(letra, end=" ")
+print("Palavra:")
+print(" ".join(palavraUsuario))
 print("\n")
 
-acertos = 0    
+acertos = 0
 erros = 0
 letrasTentadas = []
 
 while acertos != len(palavra) and erros < 4:
 
-    tentativa = input("Digite uma letra: ")
+    print("\nO que você deseja fazer?")
+    print("1 - Tentar uma letra")
+    print("2 - Tentar adivinhar a palavra inteira")
 
-    if tentativa == "" or tentativa == " ":
+    escolha = input("Escolha 1 ou 2: ").strip()
+
+    if escolha != "1" and escolha != "2":
+        print("Opção inválida! Escolha 1 ou 2.\n")
         continue
 
-    letraTentativa = tentativa[0]
-    letraTentativa = letraTentativa.upper()
+    # Tentar a palavra inteira
+    if escolha == "2":
+        palpite = input("Digite a palavra completa: ").strip().upper()
 
-    if letraTentativa in letrasTentadas:
-        print("Você já tentou a letra", letraTentativa)
-        continue
-            
-    letrasTentadas.append(letraTentativa)
-        
-    acertouNaRodada = False
-        
-    i = 0
-    while i < len(palavra):
-        if letraTentativa == palavra[i]:
-            palavraUsuario[i] = palavra[i]
-            acertouNaRodada = True
-        i = i + 1
-        
-    if acertouNaRodada == True:
-        print("Acertou!")
-        acertos = 0
-        for letra in palavraUsuario:
-            if letra != "_":
-                acertos = acertos + 1
+        invalido = False
+        for caractere in palpite:
+            if caractere < "A" or caractere > "Z":
+                invalido = True
+
+        if invalido:
+            print("Entrada inválida! Use apenas letras sem acento.\n")
+            continue
+
+        if palpite == palavra:
+            palavraUsuario = list(palavra)
+            acertos = len(palavra)
+            break
+        else:
+            print("Palpite errado!")
+            erros += 1
+
+    # Tentar uma letra
     else:
-        erros = erros + 1
-        print("Errou!")
-            
+        tentativa = input("Digite uma letra: ").strip().upper()
+
+        if len(tentativa) == 0:
+            continue
+
+        letraTentativa = tentativa[0]
+
+        if letraTentativa < "A" or letraTentativa > "Z":
+            print("Entrada inválida! Use apenas letras sem acento.\n")
+            continue
+
+        if letraTentativa in letrasTentadas:
+            print("Você já tentou essa letra!")
+            continue
+
+        letrasTentadas.append(letraTentativa)
+
+        acertou = False
+        for i in range(len(palavra)):
+            if palavra[i] == letraTentativa:
+                palavraUsuario[i] = letraTentativa
+                acertou = True
+
+        if acertou:
+            print("Acertou!")
+        else:
+            print("Errou!")
+            erros += 1
+
+    # Contar acertos
+    acertos = 0
+    for l in palavraUsuario:
+        if l != "_":
+            acertos += 1
+
+    # Desenho da forca
     if erros == 1:
         print("  _______")
         print(" |/      |")
@@ -103,8 +149,8 @@ while acertos != len(palavra) and erros < 4:
         print(" |/      |")
         print(" |      (_)")
         print(" |      \\|/")
-        print(" |")
-        print(" |")
+        print(" | ")
+        print(" | ")
         print(" |")
         print("_|___ \n")
     elif erros == 3:
@@ -113,7 +159,7 @@ while acertos != len(palavra) and erros < 4:
         print(" |      (_)")
         print(" |      \\|/")
         print(" |       |")
-        print(" |")
+        print(" | ")
         print(" |")
         print("_|___ \n")
     elif erros == 4:
@@ -126,13 +172,12 @@ while acertos != len(palavra) and erros < 4:
         print(" |")
         print("_|___ \n")
 
-    print("\nPalavra Atual:")
-    for letra in palavraUsuario:
-        print(letra, end=" ")
-    print()
-    print("Erros:", erros, "/4. Letras Tentadas:", letrasTentadas)
+    print("Palavra Atual:")
+    print(" ".join(palavraUsuario))
+    print("Letras tentadas:", letrasTentadas)
+    print(f"Erros: {erros}/4\n")
 
 if acertos == len(palavra):
-    print("\nParabéns! Você venceu!")
+    print("Parabéns! Você venceu!")
 else:
-    print("\nFim de jogo! A palavra era:", palavra)
+    print("Fim de jogo! A palavra era:", palavra)
