a3da9e98c6b2fa742f6434d49f3b01da638cf9d6
tpires1041 <tiagopires2501@outlook.com>
2025-11-18 01:22:44 -0300

    Adicionada funcionalidade
    Adicionados os níveis e também tirei a possibilidade das palavras se repetirem entre os níveis

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ print("------------------------------")
 print("|        JOGO DA FORCA       |")
 print("------------------------------\n")
 
-print("Escolha um tema")
-print("1 - Profissões")
-print("2 - Objetos")
-print("3 - Meios de transporte")
-print("4 - Animais")
-print("5 - Sair\n")
-
-opcao = input("Escolha uma opção: ")
-
 temaProfissoes = ["ADVOGADO", "PILOTO", "MEDICO", "ENGENHEIRO", "PROFESSOR", "PROGRAMADOR",
                   "ENFERMEIRO", "FARMACEUTICO", "ARQUITETO", "DESIGNER", "MOTORISTA",
                   "VENDEDOR", "GERENTE", "ARTISTA", "PSICOLOGO", "BIOLOGO", "QUIMICO",
@@ -28,14 +19,23 @@ temaTransporte = ["CARRUAGEM", "CANOA", "BONDE", "TUKTUK", "BALAO", "CAMELO", "C
 temaAnimais = ["CACHORRO", "GATO", "ELEFANTE", "TARTARUGA", "PATO", "ARARA", "MACACO",
                "GIRAFA", "TIGRE", "LEAO", "PANTERA"]
 
+print("\nEscolha um tema")
+print("1 - Profissões")
+print("2 - Objetos")
+print("3 - Meios de transporte")
+print("4 - Animais")
+print("5 - Sair\n")
+
+opcao = input("Escolha uma opção: ")
+
 if opcao == "1":
-        palavra = random.choice(temaProfissoes)
+    lista = temaProfissoes
 elif opcao == "2":
-        palavra = random.choice(temaObjetosdia)
+    lista = temaObjetosdia
 elif opcao == "3":
-    palavra = random.choice(temaTransporte)
+    lista = temaTransporte
 elif opcao == "4":
-    palavra = random.choice(temaAnimais)
+    lista = temaAnimais
 elif opcao == "5":
     print("Saindo...")
     exit()
@@ -43,141 +43,155 @@ else:
     print("Opção inválida!")
     exit()
 
-print("\nPalavra selecionada! Vamos jogar!\n")
+for fase in range(1, 5):
 
-print("  _______")
-print(" |/      |")
-print(" | ")
-print(" | ")
-print(" | ")
-print(" | ")
-print(" |")
-print("_|___ \n")
+    palavra = random.choice(lista)
+    lista.remove(palavra)
 
-palavraUsuario = ["_"] * len(palavra)
+    print(f"\n------------ FASE {fase} ------------\n")
+    print("Palavra selecionada! Vamos jogar!\n")
 
-print("Palavra:")
-print(" ".join(palavraUsuario))
-print("\n")
+    print("  _______")
+    print(" |/      |")
+    print(" | ")
+    print(" | ")
+    print(" | ")
+    print(" | ")
+    print(" |")
+    print("_|___ \n")
 
-acertos = 0
-erros = 0
-letrasTentadas = []
+    palavraUsuario = []
+    for i in range(len(palavra)):
+        palavraUsuario.append("_")
 
-while acertos != len(palavra) and erros < 4:
+    print("Palavra:")
+    for letra in palavraUsuario:
+        print(letra, end=" ")
+    print()
 
-    print("\nO que você deseja fazer?")
-    print("1 - Tentar uma letra")
-    print("2 - Tentar adivinhar a palavra inteira")
+    print("\n")
 
-    escolha = input("Escolha 1 ou 2: ").strip()
-
-    if escolha != "1" and escolha != "2":
-        print("Opção inválida! Escolha 1 ou 2.\n")
-        continue
-
-    # Tentar a palavra inteira
-    if escolha == "2":
-        palpite = input("Digite a palavra completa: ").strip().upper()
-
-        invalido = False
-        for caractere in palpite:
-            if caractere < "A" or caractere > "Z":
-                invalido = True
-
-        if invalido:
-            print("Entrada inválida! Use apenas letras sem acento.\n")
-            continue
-
-        if palpite == palavra:
-            palavraUsuario = list(palavra)
-            acertos = len(palavra)
-            break
-        else:
-            print("Palpite errado!")
-            erros += 1
-
-    # Tentar uma letra
-    else:
-        tentativa = input("Digite uma letra: ").strip().upper()
-
-        if len(tentativa) == 0:
-            continue
-
-        letraTentativa = tentativa[0]
-
-        if letraTentativa < "A" or letraTentativa > "Z":
-            print("Entrada inválida! Use apenas letras sem acento.\n")
-            continue
-
-        if letraTentativa in letrasTentadas:
-            print("Você já tentou essa letra!")
-            continue
-
-        letrasTentadas.append(letraTentativa)
-
-        acertou = False
-        for i in range(len(palavra)):
-            if palavra[i] == letraTentativa:
-                palavraUsuario[i] = letraTentativa
-                acertou = True
-
-        if acertou:
-            print("Acertou!")
-        else:
-            print("Errou!")
-            erros += 1
-
-    # Contar acertos
     acertos = 0
-    for l in palavraUsuario:
-        if l != "_":
-            acertos += 1
+    erros = 0
+    letrasTentadas = []
 
-    # Desenho da forca
-    if erros == 1:
-        print("  _______")
-        print(" |/      |")
-        print(" |      (_)")
-        print(" | ")
-        print(" | ")
-        print(" | ")
-        print(" |")
-        print("_|___ \n")
-    elif erros == 2:
-        print("  _______")
-        print(" |/      |")
-        print(" |      (_)")
-        print(" |      \\|/")
-        print(" | ")
-        print(" | ")
-        print(" |")
-        print("_|___ \n")
-    elif erros == 3:
-        print("  _______")
-        print(" |/      |")
-        print(" |      (_)")
-        print(" |      \\|/")
-        print(" |       |")
-        print(" | ")
-        print(" |")
-        print("_|___ \n")
-    elif erros == 4:
-        print("  _______")
-        print(" |/      |")
-        print(" |      (_)")
-        print(" |      \\|/")
-        print(" |       |")
-        print(" |      / \\")
-        print(" |")
-        print("_|___ \n")
+    while acertos != len(palavra) and erros < 4:
 
-    print("Palavra Atual:")
-    print(" ".join(palavraUsuario))
-    print("Letras tentadas:", letrasTentadas)
-    print(f"Erros: {erros}/4\n")
+        print("\nO que você deseja fazer?")
+        print("1 - Tentar uma letra")
+        print("2 - Tentar adivinhar a palavra inteira")
 
-if acertos == len(palavra):
-    print("Parabéns! Você venceu!")
-else:
-    print("Fim de jogo! A palavra era:", palavra)
+        escolha = input("Escolha 1 ou 2: ").strip()
+
+        if escolha != "1" and escolha != "2":
+            print("Opção inválida! Escolha 1 ou 2.\n")
+            continue
+
+        if escolha == "2":
+            palpite = input("Digite a palavra completa: ").strip().upper()
+
+            invalido = False
+            for caractere in palpite:
+                if caractere < "A" or caractere > "Z":
+                    invalido = True
+
+            if invalido:
+                print("Entrada inválida! Use apenas letras sem acento.\n")
+                continue
+
+            if palpite == palavra:
+                palavraUsuario = list(palavra)
+                acertos = len(palavra)
+                break
+            else:
+                print("Palpite errado!")
+                erros += 1
+
+        else:
+            tentativa = input("Digite uma letra: ").strip().upper()
+
+            if len(tentativa) == 0:
+                continue
+
+            letraTentativa = tentativa[0]
+
+            if letraTentativa < "A" or letraTentativa > "Z":
+                print("Entrada inválida! Use apenas letras sem acento.\n")
+                continue
+
+            if letraTentativa in letrasTentadas:
+                print("Você já tentou essa letra!")
+                continue
+
+            letrasTentadas.append(letraTentativa)
+
+            acertou = False
+            for i in range(len(palavra)):
+                if palavra[i] == letraTentativa:
+                    palavraUsuario[i] = letraTentativa
+                    acertou = True
+
+            if acertou:
+                print("Acertou!")
+            else:
+                print("Errou!")
+                erros += 1
+
+        acertos = 0
+        for l in palavraUsuario:
+            if l != "_":
+                acertos += 1
+
+        if erros == 1:
+            print("  _______")
+            print(" |/      |")
+            print(" |      (_)") 
+            print(" | ")
+            print(" | ")
+            print(" | ")
+            print(" |")
+            print("_|___ \n")
+        elif erros == 2:
+            print("  _______")
+            print(" |/      |")
+            print(" |      (_)") 
+            print(" |      \\|/")
+            print(" | ")
+            print(" | ")
+            print(" |")
+            print("_|___ \n")
+        elif erros == 3:
+            print("  _______")
+            print(" |/      |")
+            print(" |      (_)") 
+            print(" |      \\|/")
+            print(" |       |")
+            print(" | ")
+            print(" |")
+            print("_|___ \n")
+        elif erros == 4:
+            print("  _______")
+            print(" |/      |")
+            print(" |      (_)") 
+            print(" |      \\|/")
+            print(" |       |")
+            print(" |      / \\")
+            print(" |")
+            print("_|___ \n")
+
+        print("Palavra Atual:")
+        for letra in palavraUsuario:
+            print(letra, end=" ")
+        print()
+
+        print("Letras tentadas:", letrasTentadas)
+        print(f"Erros: {erros}/4\n")
+
+    if acertos == len(palavra):
+        print("Parabéns! Você venceu a fase", fase)
+    else:
+        print("Fim de jogo! A palavra era:", palavra)
+        print("Você perdeu na fase", fase)
+        exit()
+
+print("Parabéns! Você passou por todas as fases!")
